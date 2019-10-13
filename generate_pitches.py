d40d94da1a4747683c45ce4798100242e4eb54bf
root <winson.luk@gmail.com>
2019-10-13 12:15:47 -0700

    update

--- generate_pitches.py
+++ generate_pitches.py
@@ -6,14 +6,13 @@ import gpt_2_simple as gpt2
 sess = gpt2.start_tf_sess()
 gpt2.load_gpt2(sess)
 
-for _ in range(1000):
+while True:
     with open('original_pitches.txt') as out:
         f = io.StringIO()
         with redirect_stdout(f):
             gpt2.generate(sess,
                 length=1000,
                 top_p=0.2,
-                nsamples=10,
                 temperature=2.0,
             )
         content = [x.strip() for x in out.readlines()]
@@ -21,7 +20,7 @@ for _ in range(1000):
             line = line.strip()
             line_arr = line.split(' ')
             if len(line_arr) > 3 and all([line.lower() not in [w.lower() for w in content],
-                    all(x.isalpha() or x.isspace() or x.isnumeric() or x in ['.'] for x in line),
+                    all(x.isalpha() or x.isspace() for x in line),
                     len(line) < 70 and len(line) > 40,
                     line.count('.') < 2,
                     sum(list(map(lambda x:1 if x.isdigit() else 0,set(line)))) < 2,
