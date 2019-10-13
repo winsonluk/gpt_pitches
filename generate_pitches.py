344c8c19886add36730b51d4ab4ed5c30d0a3815
root <winson.luk@gmail.com>
2019-10-13 11:42:23 -0700

    update

--- generate_pitches.py
+++ generate_pitches.py
@@ -20,13 +20,12 @@ for _ in range(1000):
         for line in f.getvalue().split('\n')[1:-1]:
             line = line.strip()
             line_arr = line.split(' ')
-            if all([line.lower() not in [w.lower() for w in content],
+            if len(line_arr) > 3 and all([line.lower() not in [w.lower() for w in content],
                     all(x.isalpha() or x.isspace() or x.isnumeric() or x in ['.'] for x in line),
                     len(line) < 70 and len(line) > 40,
                     line.count('.') < 2,
                     sum(list(map(lambda x:1 if x.isdigit() else 0,set(line)))) < 2,
                     all([w[-1] == w[-1].lower() for w in line.split(' ') if len(w) > 1]),
-                    len(line_arr) > 3,
                     line_arr[0] != 'I',
                     line_arr[1].lower()[-1] != 's',
                     ]):
